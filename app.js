a836086a81eef8b5b46900f1b54e7b335db9d311
techdave <klatzdavie19@gmail.com>
2022-08-01 09:16:11 +0300

    handle POST requests

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
+const { StatusCodes } = require('http-status-codes');
+
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -8,8 +11,25 @@ const tours = JSON.parse(
 
 app.get('/api/v1/tours', (req, res) => {
   res
-    .status(200)
+    .status(StatusCodes.OK)
     .json({ status: 'success', results: tours.length, data: { tours } });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) =>
+      res.status(StatusCodes.CREATED).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      })
+  );
 });
 
 const port = 5000;
