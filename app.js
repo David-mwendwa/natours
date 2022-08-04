b7f20b2ecda687b99f058768ed78575383dd2cd1
techdave <klatzdavie19@gmail.com>
2022-08-04 18:36:06 +0300

    create _get tour by Id_ route

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ app.get('/api/v1/tours', (req, res) => {
     .json({ status: 'success', results: tours.length, data: { tours } });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  let tour = tours.find(tour => tour.id === id)
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+  res.status(StatusCodes.OK).json({ status: 'success', date: {tour} });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
