b8b6d1e5d27e2706aac92d38824d5d4212ea9e15
techdave <klatzdavie19@gmail.com>
2022-08-25 11:47:17 +0300

    serve static files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 // MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`)); //serve html files
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
