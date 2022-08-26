a7bfdc20816588c10d2af2f646a8e9cae4e5cb91
techdave <klatzdavie19@gmail.com>
2022-08-26 12:51:23 +0300

    connect mongoDB with express

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' }); //specify where config.env is located
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB).then(() => console.log('DB connection successfull'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
