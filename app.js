cfc7af314ac1141e8332eed533633b01ca434118
techdave <klatzdavie19@gmail.com>
2022-08-29 09:12:02 +0300

    errorh: handle unhandle routes

--- app.js
+++ app.js
@@ -8,14 +8,8 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`)); //serve html files
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -25,5 +19,14 @@ app.use((req, res, next) => {
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res
+    .status(404)
+    .json({
+      status: 'fail',
+      message: `Can't find ${req.originalUrl} on this server`,
+    });
+});
 
 module.exports = app;
