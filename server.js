4a81fb6cca92c2586ee8837de24ddcc3d27d39d3
techdave <klatzdavie19@gmail.com>
2022-08-29 13:41:25 +0300

    errorh: catch uncaught exceptions

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const app = require('./app');
 
+// handle error that occur in synchronous code but are no handled anywhere
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log(`UNCAUGHT EXCEPTION! Shutting down...`);
+  server.close(() => process.exit(1));
+});
+
 dotenv.config({ path: './config.env' }); //specify where config.env is located
 
 const DB = process.env.DATABASE.replace(
@@ -15,8 +22,10 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
+// handle errors occuring outside express
 process.on('unhandledRejection', (err) => {
   console.log(err.name, err.message);
   console.log(`UNHANDLED REJECTION! Shutting down...`);
   server.close(() => process.exit(1));
 });
+
