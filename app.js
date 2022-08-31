49fd72d391a66c0fd2b31d2dac041724d7c78bab
techdave <klatzdavie19@gmail.com>
2022-08-31 15:01:18 +0300

    prevent parameter pollution

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('xpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -28,9 +29,14 @@ app.use('/api', limiter);
 // body parser, reading data from req.body
 app.use(express.json());
 
-// data sanitization against xss
+// data sanitization against NoSQL query injection
 app.use(mongoSanitize());
+
+// data sanitization against XSS
 app.use(xss());
+
+// prevent parameter pollution
+app.use(hpp());
 
 // serving static files
 app.use(express.static(`${__dirname}/public`)); //serve html files
