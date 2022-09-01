79ac457bd15b3d5b671ce65dad2a2e86d535e601
techdave <klatzdavie19@gmail.com>
2022-09-01 17:58:54 +0300

    fix a bug on getAll factory function

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -65,7 +65,7 @@ exports.getAll = (Model) =>
     let filter = {};
     if (req.params.tourId) filter = { tour: req.params.tourId };
 
-    const features = new APIFeatures(Model.find(), req.query)
+    const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
       .sort()
       .limitFields()
