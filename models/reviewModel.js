783eb9c8fdd43d327a67a9a5d0d369ca782bae13
techdave <klatzdavie19@gmail.com>
2022-09-03 10:26:49 +0300

    add an index for slug

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -49,4 +49,29 @@ reviewSchema.pre(/^find/, async function (next) {
   next();
 });
 
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRatings: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  console.log(stats);
+  Tour.findByIdAndUpdate(tourId, {
+    
+  })
+};
+
+reviewSchema.post('save', function () {
+  // this points to current review
+  this.constructor.calcAverageRatings(this.tour);
+  
+});
+
 module.exports = mongoose.model('Review', reviewSchema);
