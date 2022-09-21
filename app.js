adaa44af5ca810a5649242f7e6390f8a44f10704
techdave <klatzdavie19@gmail.com>
2022-09-21 08:36:02 +0300

    extend base template with blocks

--- app.js
+++ app.js
@@ -55,6 +55,17 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.status(200).render('base', { tour: 'The Forest Hiker', user: 'David' });
 });
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
