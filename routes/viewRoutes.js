4e276f5d4da80c41afb17ad01b2679eb9e997010
techdave <klatzdavie19@gmail.com>
2022-09-24 10:53:15 +0300

    fix index page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,11 +3,7 @@ const { getOverview, getTour } = require('../controllers/viewsController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).render('base', { tour: 'The Forest Hiker', user: 'David' });
-});
-
-router.get('/overview', getOverview);
+router.get('/', getOverview);
 
 router.get('/tour', getTour);
 
